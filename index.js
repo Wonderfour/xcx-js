dbf9fa9844ed90fb4689f3c1a83c771e4a9aed5e
limeng <lmg1126@qq.com>
2019-04-01 17:49:38 +0800

    fix:提交最新的修改

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
-
-const pool = mysql.createPool({
-    host     : '127.0.0.1',  // 主机名
-    port     : 3306, // 数据库连接的端口号 默认是3306
-    database : 'it-zhihu', // 需要查询的数据库
-    user     : 'root', // 用户名
-    password : 'root1126' // 密码，我的密码是空。所以是空字符串
-});
-// const sqlData = require('./sql/mysql');
+const adminRouter = require('./admin/index')
+const xcxRouter = require('./app/index')
 var myLogger = function (req, res, next) {
   console.log('LOGGED')
   next()
 }
 
 app.use(myLogger)
-app.get('/',(req,res) =>{
-  pool.getConnection(function (err,connection) { // 使用连接池
-    if(err){
-        console.log('与MySQL数据库建立连接失败！');
-        console.log('错误信息为：' + err);
-    }else{
-      connection.query();
-      console.log('链接成功');
-    }});
-  res.send('首页数据');
-})
-app.get('/test',(req,res) =>{
-  res.send('hello World');
-})
-app.get('/about',(req,res) =>{
-  res.send('hello World2222');
-})
-
-app.listen('3000', () =>{
-  console.log('端口号：3000')
+app.use('/admin',adminRouter);
+app.use('/xcx',xcxRouter);
+app.listen('80', () =>{
 })
--- sql/mysql.js
+++ sql/mysql.js
@@ -1,9 +1,9 @@
-var mysql = require('mysql'); // mysql模块 同样可以使用 npm install -g mysql 来全局下载
-var pool = mysql.createPool({
-    host     : 'localhost',  // 主机名
+const mysql = require('mysql');
+const pool = mysql.createPool({
+    host     : '118.25.74.164',  // 主机名
     port     : 3306, // 数据库连接的端口号 默认是3306
     database : 'it-zhihu', // 需要查询的数据库
     user     : 'root', // 用户名
-    password : '' // 密码，我的密码是空。所以是空字符串
+    password : 'It-zhihu1126' // 密码，我的密码是空。所以是空字符串
 });
 module.exports = pool
