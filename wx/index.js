2128a6ceaa9ffd2060adab2c5a6e75d6c81e4efa
limeng <lmg1126@qq.com>
2019-06-12 19:33:14 +0800

    fix:再次提交代码

--- wx/index.js
+++ wx/index.js
@@ -4,6 +4,15 @@ const connection = require('../sql/mysql');
 let request = require('request');
 let config = require('./config');
 router.get('/index',(req,res) =>{
-  res.send(req);
+  console.log(req.query);
+  let signature = req.query.signature;
+  let timestamp = req.query.timestamp;
+  let nonce = req.query.nonce;
+  res.send(req.query);
 })
+function checkSignature (signature,timestamp,nonce){
+  let tempArr = [timestamp];
+  tempArr.push(nonce);
+  this
+}
 module.exports = router;
