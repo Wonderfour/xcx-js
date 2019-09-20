607ffd1e33e981d0719862983f0c69a9e764330e
meng.li1 <meng.li1@ly.com>
2019-09-20 13:39:50 +0800

    fix:新增https

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cookieParser=require("cookie-parser");
 const adminRouter = require('./admin/index')
 const xcxRouter = require('./app/index')
 const wxRouter = require('./wx/index')
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 require('./utils/Date')
 
 // var myLogger = function (req, res, next) {
@@ -12,9 +15,18 @@ require('./utils/Date')
 // }
 // app.use(myLogger)
 
+const httpsOptions = {
+  key: fs.readFileSync('./https/2834889_www.wonderfour.cn.key'),
+  cert: fs.readFileSync('./https/2834889_www.wonderfour.cn.pem')
+}
+
+httpsServer = https.createServer(httpsOptions,app);
+httpServer = http.createServer(app);
+
 app.use(cookieParser());
 app.use('/admin',adminRouter);
 app.use('/xcx',xcxRouter);
 app.use('/wx',wxRouter);
-app.listen('80', () =>{
-})
+
+httpsServer.listen(443);
+httpServer.listen(80);
